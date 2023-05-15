9f7b6c6d200d4370270fcb2a15f9df29be1a5121
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-05-14 21:05:58 -0300

    Highlighted section

--- bitcent/src/components/landing/highlighted/index.tsx
+++ bitcent/src/components/landing/highlighted/index.tsx
@@ -1,9 +1,20 @@
 import Area from "../common/Area";
+import Slogan from "./Slogan";
+import main from "../../../../public/main.jpg"
+import ResponsiveImage from "../common/responsiveImage";
 
 export default function Highlighted() {
     return (
-        <Area id="start">
-            
+        <Area id="start" className="pt-20">
+            <div className={`
+                h-[500px] flex items-center justify-around
+            `}>
+                <Slogan />
+                <ResponsiveImage
+                    image={main}
+                    className="rotate-3 hidden md:inline"
+                />
+            </div>
         </Area>
     )
 }
