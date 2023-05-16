996f20f39738b54c51be2959743f84977a9fdada
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-05-16 12:50:38 -0300

    unformat numbers

--- bitcent/src/logic/utils/MoneyFormatted.ts
+++ bitcent/src/logic/utils/MoneyFormatted.ts
@@ -7,4 +7,10 @@ export default class MoneyFormatted {
             style: "currency", currency: MoneyFormatted._moeda
         })
     }
+
+    static unFormat(value: string): number {
+        const nums = value.replace(/[^0-9]+/g, "")
+        const i = nums.length - 2
+        return Number(`${nums.substring(0, i)}.${nums.substring(i)}`)
+    }
 }
