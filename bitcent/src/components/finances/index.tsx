def3c64970c9c44e5d06dc10302321b914337e6a
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-05-18 20:48:55 -0300

    responsive fix

--- bitcent/src/components/finances/index.tsx
+++ bitcent/src/components/finances/index.tsx
@@ -25,17 +25,22 @@ export default function Finances() {
                     data={data}
                     dataMudou={alterarData}
                 />
-                <div className="flex gap-5">
+                <div className="flex gap-5 items-center">
                     <Button
-                        className="bg-blue-500"
-                        leftIcon={<IconPlus />}
+                        className="bg-blue-500 flex gap-10"
                         onClick={() => select(emptyTransaction)}
-                    >Nova transação</Button>
+                    >
+                        <span className="flex items-center gap-3">
+                            <IconPlus />
+                            <span className="hidden md:inline">Nova Transação</span>
+                        </span>
+                    </Button>
                     <SegmentedControl
                         data={[
                             { label: <IconList />, value: 'lista' },
                             { label: <IconLayoutGrid />, value: 'grade' }
                         ]}
+                        className="hidden md:inline-flex"
                         onChange={tipo => alterarExibicao(tipo as TipoExibicao)}
                     />
                 </div>
